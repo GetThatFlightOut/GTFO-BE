ed5db555b71b04419390f731f6adccfcc75f7cb6
George Soderholm <georgesoderholm@gmail.com>
2021-01-10 16:40:33 -0700

    Remove pry
    Co-authored-by: Jesse Mellinger <29828129+JesseMellinger@users.noreply.github.com>

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,7 +1,9 @@
 class Api::V1::SearchController < ApplicationController
   def index
     trips = TripFacade.get_trips(flight_params)
+
     render json: TripSerializer.new(trips)
+    # binding.pry
   end
 
   def flight_params

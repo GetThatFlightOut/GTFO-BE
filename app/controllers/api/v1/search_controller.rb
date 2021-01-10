9042d2d33ae60cef60d19892bf5a67180c776df9
George Soderholm <georgesoderholm@gmail.com>
2021-01-10 12:08:47 -0700

    Add to the index method of the SearchController
    Co-authored-by: Jesse Mellinger <29828129+JesseMellinger@users.noreply.github.com>

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SearchController < ApplicationController
   def index
-    require 'pry'; binding.pry
+    trips = TripFacade.get_trips(flight_params)
+    render json: TripSerializer.new(trips)
+  end
+
+  def flight_params
+    params.permit(:departure_airport, :departure_date, :trip_duration, :limit)
   end
 end

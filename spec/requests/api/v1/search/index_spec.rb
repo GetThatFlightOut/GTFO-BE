4101f24697747b480ec4afc2d192993fe4b9fdb6
todd-estes <elestes@gmail.com>
2021-01-11 22:27:12 -0700

    Add Get Lucky flight selection test to index spec
    Co-authored-by: Will Dunlap <53274357+dunlapww@users.noreply.github.com>

--- spec/requests/api/v1/search/index_spec.rb
+++ spec/requests/api/v1/search/index_spec.rb
@@ -56,6 +56,34 @@ RSpec.describe 'Search', :vcr do
     expect(response[:error]).to eq('Invalid Data')
   end
 
-  xit 'returns an error message when the flight_params have invalid dates'
+  xit 'returns an error message when the flight_params have invalid dates' do
+  end
 
+  it 'returns one flight when lucky search request is received from front end' do
+    flight_params = {
+            :departure_airport => 'DEN',
+            :departure_date => '21/03/2021',
+            :trip_duration => 5,
+            :limit => 1
+            }
+    get '/api/v1/search', params: flight_params
+    trips = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response.body).to be_a String
+    expect(response).to be_successful
+    expect(trips[:data]).to be_an Array
+    expect(trips[:data].count).to eq(1)
+    # expect(trips[:data].count).to_not eq(10)
+    expect(trips[:data][0][:attributes][:weather].count).to eq(8)
+    expect(trips[:data][0][:id]).to eq('0')
+    expect(trips[:data][0][:id]).to_not eq(nil)
+    expect(trips[:data][0][:attributes]).to be_a Hash
+    expect(trips[:data][0][:attributes][:origin_city]).to be_a String
+    expect(trips[:data][0][:attributes][:price]).to be_a Integer
+    expect(trips[:data][0][:attributes][:booking_link]).to be_a String
+    expect(trips[:data][0][:attributes][:min_f]).to be_a Float
+    expect(trips[:data][0][:attributes][:day_feels_like_c]).to be_a Float
+    expect(trips[:data][0][:attributes][:weather]).to be_an Array
+    expect(trips[:data][0][:attributes][:weather]).to_not be_a Hash
+  end
 end

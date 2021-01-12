09ab762f0b8187639060cf2f822aa2b4903b8aa6
Aidan Murray <aidanhansm@gmail.com>
2021-01-12 16:08:50 -0700

    Revert "Merge branch 'asyncronous-api-calls' of github.com:GetThatFlightOut/GTFO-BE into asyncronous-api-calls"
    This reverts commit e01aaa2b26aa0c1786f624943cd0ee96532b1d31, reversing
    changes made to 709034fc7fb3354b35b3f4d19a18fe68768b5f21.

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -2,12 +2,15 @@ class TripFacade
   def self.get_trips(flight_params)
     flights = FlightService.get_flights(flight_params)
     @trips = []
+
     if flights[:message].nil?
+      mutex = Mutex.new
       threads = []
       flights[:data].each_with_index do |flight, index|
         threads << Thread.new do
           Rails.application.executor.wrap do
             t = Thread.current
+            # Thread.start(t)
 
             lat = flight[:attributes][:latitude]
             lon = flight[:attributes][:longitude]
@@ -16,11 +19,18 @@ class TripFacade
           end
         end
       end
+      # require 'pry'; binding.pry
+      threads.map(&:value).flatten
+      # require 'pry'; binding.pry
       threads.each do |t|
+        # require 'pry'; binding.pry
         @trips << t[:trip]
         t.exit
       end
-      @trips #return value: an array of Trip Objects
+      # require 'pry'; binding.pry
+      # render text: "Finished #{flights.length} blocking operations asynchronously."
+      @trips # return value: an array of Trip Objects
+
     else
       flights
     end

8024779e49ed72a46880fb01f116b19075a6b3ae
Aidan Murray <aidanhansm@gmail.com>
2021-01-12 15:45:57 -0700

    Implement threads properly

--- app/facades/trip_facade.rb
+++ app/facades/trip_facade.rb
@@ -1,30 +1,35 @@
 class TripFacade
   def self.get_trips(flight_params)
     flights = FlightService.get_flights(flight_params)
-    trips = []
+    @trips = []
 
     if flights[:message].nil?
-      # mutex = Mutex.new
+      mutex = Mutex.new
       threads = []
       flights[:data].each_with_index do |flight, index|
         threads << Thread.new do
-          t = Thread.current
-          # mutex.synchronize do
-          t[:lat] = flight[:attributes][:latitude]
-          t[:lon] = flight[:attributes][:longitude]
-          t[:weather] = WeatherService.get_weather(lat, lon)
-          t[:trips] << Trip.new(flight, weather, index)
-          sleep 0.5
-          # end
+          Rails.application.executor.wrap do
+            t = Thread.current
+            # Thread.start(t)
+
+            lat = flight[:attributes][:latitude]
+            lon = flight[:attributes][:longitude]
+            weather = WeatherService.get_weather(lat, lon)
+            t[:trip] = Trip.new(flight, weather, index)
+          end
         end
       end
-      binding.pry
+      # require 'pry'; binding.pry
+      threads.map(&:value).flatten
+      # require 'pry'; binding.pry
       threads.each do |t|
-        t.join
-        t.pass
+        # require 'pry'; binding.pry
+        @trips << t[:trip]
+        t.exit
       end
+      # require 'pry'; binding.pry
       # render text: "Finished #{flights.length} blocking operations asynchronously."
-      trips #return value: an array of Trip Objects
+      @trips # return value: an array of Trip Objects
 
     else
       flights

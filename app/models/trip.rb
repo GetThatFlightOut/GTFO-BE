27d8c2ad10f0a027237f83d9730d185f51cda966
Brian Liu <brian.b.liu@gmail.com>
2021-01-13 16:52:38 -0700

    Delete id attribute, cleanup
    Co-authored-by: Aidan Murray 
    <48848183+TeknoServal@users.noreply.github.com>

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,35 +5,8 @@ class Trip < ApplicationRecord
   has_many :request_trips
   has_many :requests, through: :request_trips
 
-  # attr_reader :id,
-  #             :origin_city,
-  #             :destination_city,
-  #             :origin_iata,
-  #             :destination_iata,
-  #             :latitude,
-  #             :longitude,
-  #             :price,
-  #             :departure_datetime,
-  #             :arrival_datetime,
-  #             :r_departure_datetime,
-  #             :r_arrival_datetime,
-  #             :booking_link,
-  #             :trip_duration,
-  #             :flight_id,
-  #             :min_f,
-  #             :max_f,
-  #             :min_c,
-  #             :max_c,
-  #             :day_feels_like_c,
-  #             :day_feels_like_f,
-  #             :description,
-  #             :weather_date,
-  #             :weather,
-  #             :sky_coverage
-
   def self.new_trip(flight, weather, index)
     data = {
-    id: index,
     origin_city: flight[:attributes][:origin_city],
     destination_city: flight[:attributes][:destination_city],
     origin_iata: flight[:attributes][:origin_iata],
@@ -49,17 +22,8 @@ class Trip < ApplicationRecord
     trip_duration: flight[:attributes][:trip_duration],
     flight_id: flight[:attributes][:id]
   }
-    # min_f: weather[:data][0][:attributes][:min_f], # remove
-    # max_f: weather[:data][0][:attributes][:max_f], # remove
-    # min_c: weather[:data][0][:attributes][:min_c], # remove
-    # max_c: weather[:data][0][:attributes][:max_c], # remove
-    # day_feels_like_c: weather[:data][0][:attributes][:day_feels_like_c], # remove
-    # day_feels_like_f: weather[:data][0][:attributes][:day_feels_like_f], # remove
-    # description: weather[:data][0][:attributes][:description], # remove
-    # weather_date: weather[:data][0][:attributes][:date], # remove
-    # sky_coverage: weather[:data][0][:attributes][:sky_coverage], # remove
-    weather_data = weather[:data]
 
+    weather_data = weather[:data]
 
     trip_object = create(data)
 

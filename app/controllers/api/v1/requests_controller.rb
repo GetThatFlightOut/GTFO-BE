fd014d4a9d7d0b564b2096b072a0b9bde096adac
Aidan Murray <aidanhansm@gmail.com>
2021-01-14 14:16:09 -0700

    Increase threadcount
    Co-authored-by: Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>

--- app/controllers/api/v1/requests_controller.rb
+++ app/controllers/api/v1/requests_controller.rb
@@ -1,6 +1,20 @@
 class Api::V1::RequestsController < ApplicationController
   def show
+
+    begin
     request = Request.find(params[:id])
+
+    rescue ActiveRecord::RecordNotFound
+
+      payload = {
+        error: 'There was no request that matched your search.',
+        status: 400
+      }
+
+      render json: payload, status: :bad_request
+      return
+    end
+
 
     options = {}
     options[:include] = %i[trips]
